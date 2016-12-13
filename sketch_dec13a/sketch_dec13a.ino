c9c3fb803581dc8080d9676d21a1f933e3e6b35c
GerhardHK90 <gerhard.kruger@ecampty.co.za>
2016-12-13 08:17:16 +0200

    Add blink
    Added blink to example with BLINK_DELAY

--- sketch_dec13a/sketch_dec13a.ino
+++ sketch_dec13a/sketch_dec13a.ino
@@ -1,3 +1,5 @@
+#define BLINK_DELAY 1000
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(13,OUTPUT);
@@ -5,5 +7,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(BLINK_DELAY);
+  digitalWrite(13,HIGH);
+  delay(BLINK_DELAY);
+  digitalWrite(13,LOW);
 }
